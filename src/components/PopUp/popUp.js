583e7a0d91da5e1eb882729ee36bc84a74d2c1e0
unknown <paho1993@gmail.com>
2023-11-17 12:38:26 +0100

    Birthday menu popUp

--- src/components/PopUp/popUp.js
+++ src/components/PopUp/popUp.js
@@ -1,7 +1,6 @@
 import React, { useState } from "react"
 import Modal from "react-bootstrap/Modal"
-import Button from "react-bootstrap/Button"
-import image from "../../images/wielkanoc.jpg"
+import image from "../../images/Menuurodzinowe.png"
 
 const PopUp = () => {
   const [show, setShow] = useState(true)
@@ -11,9 +10,13 @@ const PopUp = () => {
   return (
     <Modal show={show} onHide={handleClose} centered>
       <Modal.Header closeButton>
-        {/* <Modal.Title>
+        <Modal.Title>
           <b>Szanowni klienci</b>
-        </Modal.Title> */}
+          <p>
+            W ten weekend zapraszamy do lokalu na specjalne dania z okazji 3
+            urodzin Poszło z Dymem
+          </p>
+        </Modal.Title>
       </Modal.Header>
       <Modal.Body>
         <img
