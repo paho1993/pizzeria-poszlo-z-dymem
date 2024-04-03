688e7729eed26c241c41daea1a0a0cddb0c03d3b
Maciej Pacholak <paho1993@gmail.com>
2024-04-03 16:19:43 +0200

    feat: not found mage layout changes

--- src/pages/404.js
+++ src/pages/404.js
@@ -6,7 +6,7 @@ import Seo from "../components/seo"
 const NotFoundPage = () => (
   <Layout>
     <Seo title="404: Not found" />
-    <div style={{ margin: "200px" }}>
+    <div style={{ marginTop: "200px" }}>
       <h1>STRONA NIE ISTNIEJE</h1>
       <p>Ta strona już nie istnieje, wróć na stronę główną</p>
     </div>
